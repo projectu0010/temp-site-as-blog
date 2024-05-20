3e77ebccb39a2b88e0dfbc123151e9384767daea
Pradeep Sekar <pradeepsek@gmail.com>
2024-05-20 10:09:50 -0400

    updated

--- src/components/posts/posts-scroll-grid.tsx
+++ src/components/posts/posts-scroll-grid.tsx
@@ -17,22 +17,20 @@ export default function PostsScrollGrid({ allPosts }: { allPosts: Post[] }) {
   const [pageURL, setPageURL] = useState("");
   const [isMobileDevice, setIsMobileDevice] = useState(false);
   useEffect(() => {
-    setPageURL(window.location.href);
-    if (navigator.userAgent) {
+      /* Storing user's device details in a variable*/
+        let details = navigator.userAgent;
        /* Creating a regular expression  
-  containing some mobile devices keywords  
-  to search it in details string*/
-  let regexp = /android|iphone|kindle|ipad/i; 
-    
-  /* Using test() method to search regexp in details 
-  it returns boolean value*/
-  let isMobileDevice = regexp.test(details); 
+      containing some mobile devices keywords
+      to search it in details string*/
+      let regexp = /android|iphone|kindle|ipad/i;
+      /* Using test() method to search regexp in details
+      it returns boolean value*/
+      let isMobileDevice = regexp.test(details);
   setIsMobileDevice(isMobileDevice);
-    }
+
   }, []);
-  /* Storing user's device details in a variable*/
-  let details = navigator.userAgent; 
-    
+
+
  
   const settings = {
     className: "center",
